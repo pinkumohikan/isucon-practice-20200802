486395e9345012b55fe62c7002ab5d71ac26d4c4
cureseven <ogasawara.nana@willgate.co.jp>
2020-08-02 14:37:07 +0900

    index貼った

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
 #!/bin/bash -eu
 
 echo 'init process'
+
+mysql -uisucon -pisucon isucon -e "CREATE INDEX memos_idx_is_private_created_at ON memos (is_private, created_at)"
+mysql -uisucon -pisucon isucon -e "CREATE INDEX memos_idx_user_created_at ON memos (user, created_at)"
