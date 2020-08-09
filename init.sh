954d0f35ba6877d5b58c2ffa92812ed2a1db56a5
cureseven <shiningcureseven@gmail.com>
2020-08-09 16:05:31 +0900

    droinit.shの最初にp tableを入れた

--- init.sh
+++ init.sh
@@ -2,6 +2,7 @@
 
 echo 'init process'
 
+mysql -uisucon -pisucon isucon -e "drop table isucon.public_memos";
 mysql -uisucon -pisucon isucon -e "CREATE INDEX memos_idx_is_private_created_at ON memos (is_private, created_at)"
 mysql -uisucon -pisucon isucon -e "CREATE INDEX memos_idx_user_created_at ON memos (user, created_at)"
 mysql -uisucon -pisucon isucon -e "CREATE INDEX memos_idx_03 ON memos (user, is_private, created_at)"
