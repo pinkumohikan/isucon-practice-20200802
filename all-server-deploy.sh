f1bdb9596c96ce46fe7c3fd21d0a919e3fe58b15
masami <msihima0304@gmail.com>
2020-08-09 18:33:01 +0900

    perf: ⚡️ 金暴力

--- all-server-deploy.sh
+++ all-server-deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -xe
-ssh  isucon-app-1 /opt/isucon3-mod/deploy.sh & \
-ssh  isucon-app-2 /opt/isucon3-mod/deploy.sh
+ssh  isucon-app-1 /opt/isucon3-mod/deploy.sh master & \
+ssh  isucon-app-2 /opt/isucon3-mod/deploy.sh master
